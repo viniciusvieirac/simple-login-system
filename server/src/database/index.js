c1e98d9b7f68b09e27b3f6373c180a00b969987b
viniciusvieirac <viniciusvieiracdev@gmail.com>
2024-12-07 20:45:42 -0300

    feat: create migration user

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -3,11 +3,5 @@ const dbConfig = require('../config/config');
 
 const connection = new Sequelize(dbConfig);
 
-try {
-  connection.authenticate();
-  console.log('Conexao feita');
-} catch (error) {
-  console.log('erro', error)
-};
 
 module.exports = connection;
