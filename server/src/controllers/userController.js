fc0b6a2b2497a83f71a341bc6e5d38c163142187
viniciusvieirac <viniciusvieiracdev@gmail.com>
2024-12-07 23:45:04 -0300

    feat: implements token and token verify

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 module.exports = {
   async getUser(req, res) {
@@ -13,10 +22,12 @@ module.exports = {
   async create(req, res) {
     const { name, password, email } = req.body;
     const user = await User.create({ name, password, email });
+    const token = generateToken({ id: user.id });
     return res.status(201).json({
       status: 1,
       message: 'User created successfully',
       user,
+      token,
     });
   },
 
@@ -39,11 +50,13 @@ module.exports = {
     }
 
     user.password = undefined;
+    const token = generateToken({ id: user.id });
 
     return res.status(200).json({
       status: 1,
       message: 'User logged in successfully',
       user,
+      token,
     });
   }
 };
--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const UserController = require('../controllers/userController');
 const router = express.Router();
+const authMiddleware = require('../middleware/auth');
 
 
-router.get('/profile', UserController.getUser);
+router.get('/profile', authMiddleware, UserController.getUser);
 router.post('/login', UserController.login);
 router.post('/register', UserController.create);
 
